30135c6f24d16c2c1069785539e6b936a27ef5ba
JoelIGM <158236684+JoelIGM@users.noreply.github.com>
2026-09-02 22:49:43 -0600

    Update nfa.h
    Declarated types for a transition and the representation of a nfa

---
+++ regex_to_nfa/src/nfa.h
@@ -0,0 +1,16 @@
+#define EPSILON '\0'
+
+//Diferent states are represented just as a positive integer 
+typedef struct transition {
+    int start;
+    int finish;
+    char symbol; //the symbol needed to travel between states
+} transition;
+
+//We will represent the nfa as a list of transitions
+typedef struct nfa {
+    transition *transitions;
+    int start;
+    int accept;
+    int length;
+} nfa;
